01293e974b5539228ded047057f8ee98718edab8
Phil Bennett <pbennett@neworbit.co.uk>
2023-02-17 10:30:19 +0000

    grid boundaries, better colours for fixed vs playable cells

--- src/components/game/FixedSudokuSquare.tsx
+++ src/components/game/FixedSudokuSquare.tsx
@@ -1,7 +1,16 @@
 type FixedSudokuSquareProps = {
   value: number;
+  applyRightBorder: boolean;
+  applyBottomBorder: boolean;
 };
 
-export function FixedSudokuSquare({ value }: FixedSudokuSquareProps) {
-  return <div className="sudoku-square">{value}</div>;
+export function FixedSudokuSquare({
+  value,
+  applyRightBorder,
+  applyBottomBorder,
+}: FixedSudokuSquareProps) {
+  const className = `sudoku-square fixed ${
+    applyRightBorder ? "grid-col-boundary" : ""
+  } ${applyBottomBorder ? "grid-row-boundary" : ""}`;
+  return <div className={className}>{value}</div>;
 }
